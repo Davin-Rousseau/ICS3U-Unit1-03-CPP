d9ba5dd24a04c926601a835ca170a571fb4dc560
Ubuntu <ubuntu@ip-172-31-24-253.ec2.internal>
2019-09-12 21:49:01 +0000

    added program

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2019 Davin Rousseau All rights reserved
+//
+// Created by: Davin Rousseau
+// Created on September 2019
+// this is the hello world program on CPP
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+}
